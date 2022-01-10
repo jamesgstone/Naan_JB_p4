88fa9db9b2facd30db0a27725baa53c3c0a794de
james stone <jamesgstone@gmail.com>
2022-01-10 02:08:10 +0200

    added city to sql

--- database.sql
+++ database.sql
@@ -2,15 +2,23 @@ create database whiskyExchange;
 
 use whiskyExchange;
 
+
+create table city (
+id int auto_increment,
+cityName varchar(255),
+primary key (id)
+);
+
 create table users (
 id int ,
 firstname varchar(255),
 lastname varchar(255),
 email varchar(255), 
 password varchar(255),
-city varchar(255),
+cityID int,
 street varchar(255),
 role varchar(255) default "User",
+foreign key (cityID) references city(id),
 primary key (id)
 );
 
@@ -66,11 +74,14 @@ foreign key (userID) references users(id),
 primary key (id)
 );
 
-insert into users (id, firstname, lastname, email, password, city, street)
-values (1111,"Jim","Murray","user1@g.com", "123","new York","1st street"),(2222,"Michael","Jackson","user2@g.com", "123","new York","2nd street"),(3333,"Martine","Nouet","user3@g.com", "123","new York","3rd street");
+insert into city (cityName)
+values ("New York"), ("Roma"), ("Tel Aviv"), ("Paris"),("Amsterdam"),("Los Angeles"),("Herzelia"),("London"),("Montreal"),("Tokio");
 
-insert into users (id, firstname, lastname, email, password, city, street, role)
-values (5555,"Yossi","Leibman","user4@g.com","123","new York","5th avenue","Admin");
+insert into users (id, firstname, lastname, email, password, cityID, street)
+values (1111,"Jim","Murray","user1@g.com", "123",1,"1st street"),(2222,"Michael","Jackson","user2@g.com", "123",1,"2nd street"),(3333,"Martine","Nouet","user3@g.com", "123",1,"3rd street");
+
+insert into users (id, firstname, lastname, email, password, cityID, street, role)
+values (5555,"Yossi","Leibman","user4@g.com","123",1,"5th avenue","Admin");
 
 insert into category (categoryName)
 values ("Highland single malts"),
