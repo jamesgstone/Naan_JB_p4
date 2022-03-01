ed1f575335801104516c1465fae68522ee8910a4
Your NameJGS <you@example.com>
2022-03-01 17:36:49 +0200

    header search

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+searchWord ='';
+isSearching = false;
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+search() {
+  this.isSearching = true
+  
+
+}
 
 }
