601c8532a005222bf85095509c9c227209368910
Your NameJGS <you@example.com>
2022-03-08 13:16:10 +0200

    fixed services layout

--- client/src/app/services/admin.service.ts
+++ client/src/app/services/admin.service.ts
@@ -28,48 +28,6 @@ export class AdminService {
 //   "price": 1200
 // }
 
-// Create new cart / get cart id if already exist
-// GET	http://localhost:1000/cart	Request >
-// 			Response > first time
-// [{
-// "msg": "cart was created successfully"
-// }]
-// > when cart already exist
-// [{
-//         "id": 1
-// }]
-
-// Delete Cart
-// DEL	http://localhost:1000/cart/l/delete
-// 			Response >
-// {
-//     "msg": "cart was deleted successfully"
-// }
-
-// Cart Items
-
-// Get all Cart Items
-// GET	http://localhost:1000/cart/cartItem
-
-// Add product to Cart
-// GET	http://localhost:1000/cart/addtocart/:prodID
-
-// Delete Item from cart
-// DEL	http://localhost:1000/cart/deletefromcart/:prodID
-// Request > http://localhost:1000/cart/deletefromcart/1
-// Response >
-// {
-// If no such item exists in cart
-// "msg": "No Such product in cart"
-// If more than one item exists in cart
-// "msg": "one product deleted successfully"
-// If only one item exists in cart
-// "msg": "product Deleted from cart"
-// }
-
-
-
-
 
 
 
